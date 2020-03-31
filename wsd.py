3b3811783262d26a143badfa381227c9bdc66c7f
Serena Cheng <serena.ch1999@gmail.com>
2020-03-30 20:13:31 -0400

    Set up features and dict.

--- wsd.py
+++ wsd.py
@@ -22,6 +22,8 @@ model = sys.argv[3]
 train_content = []
 test_content = []
 
+sense_phone = "phone"
+sense_product = "product"
 w_plus_1 = "+1"
 w_minus_1 = "-1"
 w_pair_minus_2 = "-2 & -1"
@@ -30,37 +32,44 @@ w_pair_minus_plus_1 = "-1 & +1"
 w_window_3 = "k=3"
 w_window_5 = "k=5"
 
+frequency_dict = {w_plus_1: {}, w_minus_1: {}, w_pair_minus_2: {},
+                  w_pair_plus_2: {}, w_pair_minus_plus_1: {}, w_window_3: {},
+                  w_window_5: {}}
+
 
 def clean_context(content):
     content = re.sub(r'(<.>|<\/.>|\.|,|\")', ' ', content)
     content = re.sub(r'\s+', ' ', content)
-    content = re.sub(r'lines', "line", content)
+    content = re.sub("lines", "line", content)
     return content.split()
 
 
 with open(train_set, 'r', encoding="utf-8-sig") as file:
     train_content.extend(file.read().lower().split("</instance>"))
+    train_content.pop()
 
 with open(test_set, 'r', encoding="utf-8-sig") as file:
     test_content.extend(file.read().lower().split("</instance>"))
+    test_content.pop()
 
 for instance in train_content:
     sense = instance[instance.find("senseid=") + 9: instance.find("\"/>")]
 
-    context = instance[instance.find("<context>") + 9: instance.find(
-            "</context>")]
+    context = instance[
+              instance.find("<context>") + 9: instance.find("</context>")]
     context_words = clean_context(context)
     print(context_words)
     target = context_words.index("<head>line</head>")
-    print(target)
-    
-    break
 
+    if target + 1 < len(context_words):
+        feature_word = context_words[target + 1]
+        print(feature_word)
+        if feature_word not in frequency_dict[w_plus_1].keys():
+            frequency_dict[w_plus_1] = {feature_word: {}}
+            frequency_dict[w_plus_1][feature_word] = {sense_phone: 0,
+                                                      sense_product: 0}
+        frequency_dict[w_plus_1][feature_word][sense] += 1
 
-
-
-
-
-
-
-
+print(frequency_dict)
+# print(frequency_dict)
+# break
