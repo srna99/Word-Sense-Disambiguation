76e6314daa04d4c630e4ba72dd7612197e051f00
Serena Cheng <serena.ch1999@gmail.com>
2020-03-31 21:16:22 -0400

    Finish commenting.

--- scorer.py
+++ scorer.py
@@ -4,47 +4,50 @@ CMSC 416 - PA 4: WSD (scorer.py)
 3/31/2020
 ~~~~~
 Problem:
-This program addresses the goal of scoring the accuracy of the POS tagger.
-The overall accuracy of the implemented tagger is 0.81 or 81%.
+This program addresses the goal of scoring the accuracy of the decision list model.
+The overall accuracy of the implemented model is 0.9 or 90%.
 Usage:
 The user should include the template below when executing:
-    (tagged test data) (test key data) > (tag report stdout)
-Ex. python3 tagger.py test-with-tags.txt pos-test-key.txt > tag-report.txt
-    > Baseline Accuracy: 0.13
-    > Overall Accuracy: 0.81
-    > (confusion matrix displayed)
+    (answers to test data) (test key data)
+Ex. python3 scorer.py my-line-answers.txt line-key.txt
+    > Baseline Accuracy (phone): 0.51
+    > Overall Accuracy: 0.9
+
+    >        phone product
+    > phone     64     8
+    > product    4    50
 Algorithm:
-Each tag in the tagged test set is compared to the tag in the key and then
+Each sense in the answers set is compared to the sense in the key and then
 inputted in the confusion matrix. The baseline accuracy is calculated by
-assuming every tag to be the most frequent one (in this case it is "NN") and
-dividing the number of correct instances by the total number of tags. The
-overall accuracy is calculated by dividing the total correct tags by the
-total number of tags.
+assuming every sense to be the most frequent one (in this case it is "phone") and
+dividing the number of correct instances by the total number of answers. The
+overall accuracy is calculated by dividing the total correct senses by the
+total number of answers.
 """
 
 import sys
 import re
 
-line_answers = sys.argv[1]  # tagged test set file
+line_answers = sys.argv[1]  # answer set file
 line_key = sys.argv[2]  # test key file
 
-answer_content = []  # test broken apart
-key_content = []  # key broken apart
+answer_content = []  # test tokenized
+key_content = []  # key tokenized
 
-answers = []
-key = []
+answers = []  # senses in answer
+key = []  # senses in key
 
-senses = {"phone": 0, "product": 1}
+senses = {"phone": 0, "product": 1}  # associate sense with number
 
 
-# split tags from words and add to respective tag arrays
-def get_senses(answer_list, instance_sense_dict):
+# split senses from instances and add to respective sense lists
+def get_senses(answer_list, instance_sense_list):
     for ans in answer_list:
         sense = re.search(r'senseid=\"(.*)\"', ans).group(1)
-        instance_sense_dict.append(sense)
+        instance_sense_list.append(sense)
 
 
-# tokenize tagged test
+# tokenize answers
 with open(line_answers, 'r', encoding="utf-8-sig") as file:
     answer_content.extend(file.read().split("\n"))
     answer_content.pop()
@@ -54,16 +57,16 @@ with open(line_key, 'r', encoding="utf-8-sig") as file:
     key_content.extend(file.read().split("\n"))
     key_content.pop()
 
-# get only tags
+# get only senses
 get_senses(answer_content, answers)
 get_senses(key_content, key)
 
-# # create matrix filled with 0s that is sized (num of tags x num of tags)
+# create matrix filled with 0s that is sized (num of senses x num of senses)
 confusion_matrix = [[0] * len(senses) for _ in range(len(senses))]
 
-most_sense = ''  # most frequent tag
-most_count = 0  # correct count of frequent tag
-total_correct = 0  # total correct count of all tags
+most_sense = ''  # most frequent sense
+most_count = 0  # correct count of frequent sense
+total_correct = 0  # total correct count of all senses
 for ind in range(len(key)):
     actual_sense = key[ind]  # actual
     predicted_sense = answers[ind]  # predicted
@@ -72,34 +75,34 @@ for ind in range(len(key)):
     confusion_matrix[senses[actual_sense]][senses[predicted_sense]] += 1
 
     if actual_sense == predicted_sense:
-        # update if tag count is higher than current highest count
+        # update if sense count is higher than current highest count
         if confusion_matrix[senses[actual_sense]][senses[predicted_sense]] > most_count:
             most_sense = actual_sense
             most_count = confusion_matrix[senses[actual_sense]][senses[predicted_sense]]
 
-        # increment total correct count if tag is correct
+        # increment total correct count if sense is correct
         total_correct += 1
 
-# BA = (num of correct tags if all are most frequent tag)/total num of tags
+# BA = (num of correct answers if all are most frequent sense)/total num of answers
 baseline_accuracy = round(most_count / len(key), 2)
-# A = num of correct tags/total num of tags
+# A = num of correct answers/total num of answers
 accuracy = round(total_correct / len(key), 2)
 
-print("Baseline Accuracy:", baseline_accuracy)
+print("Baseline Accuracy (phone):", baseline_accuracy)
 print("Overall Accuracy:", accuracy)
 
 ordered_tags = [''] * len(senses)
-# put tags in order
+# put senses in order
 for tag, num in senses.items():
     ordered_tags[num] = tag
 
-# print all tags horizontally
+# print all senses horizontally
 for ind in range(len(ordered_tags)):
     if ind == 0:
         print("\n        ", end='')
     print(format(ordered_tags[ind], '>4'), end=' ')
 
-# print all tags and counts in matrix
+# print all senses and counts in matrix
 for ind in range(len(senses)):
     print("\n", format(ordered_tags[ind], '7'), end='')
     for j in range(len(senses)):
--- wsd.py
+++ wsd.py
@@ -4,55 +4,71 @@ CMSC 416 - PA 4: WSD (wsd.py)
 3/31/2020
 ~~~~~
 Problem:
-
+This program aims to disambiguate the words "line/lines" through the decision list model. Selected features
+were chosen to gain contextual information about the targeted word.
 Usage:
-
-Ex.
+The user should include additional arguments when executing program as follows:
+    (annotated training data) (test data) (model log) > (answers)
+Ex. python3 wsd.py line-train.txt line-test.txt my-model.txt > my-line-answers.txt
+    > <answer instance="line-n.w8_053:3883:" senseid="phone"/> etc.
 Algorithm:
+This model is based on Yarowsky's decision list. Unigrams and bi-grams surrounding the target word are
+collected as features and their position relative to the target word is noted. Features used includes w+1,
+w-1, w-2 & w-1, w+1 & w+2, and several more. Tests based on the features were created and ranked based on
+their log-likelihood score (abs(log( (P(sense_1|feature)/(P(sense_2|feature) ))). The tests are then applied
+to the test data in ranked order, where the first test to pass results in the associated sense.
+Baseline Accuracy (phone): 0.51
+Overall Accuracy: 0.9
 
+        phone product
+ phone     64     8
+ product    4    50
 """
 
 import sys
 import re
 import math
 
-train_set = sys.argv[1]
-test_set = sys.argv[2]
-model = sys.argv[3]
+train_set = sys.argv[1]  # training set file
+test_set = sys.argv[2]  # test set file
+model = sys.argv[3]  # model log file
 
-train_content = []
-test_content = []
+train_content = []  # tokenized training set
+test_content = []  # tokenized test set
 
+# senses
 sense_phone = "phone"
 sense_product = "product"
 
-w_plus_1 = "+1"
-w_minus_1 = "-1"
-w_pair_minus_2 = "-2 & -1"
-w_pair_plus_2 = "+1 & +2"
-w_pair_minus_plus_1 = "-1 & +1"
-w_window_3 = "k=3"
-w_window_5 = "k=5"
-w_window_10 = "k=10"
+# feature types
+w_plus_1 = "+1"  # w+1
+w_minus_1 = "-1"  # w-1
+w_pair_minus_2 = "-2 & -1"  # w-2 & w-1
+w_pair_plus_2 = "+1 & +2"  # w+1 & w+2
+w_pair_minus_plus_1 = "-1 & +1"  # w-1 & w+1
+w_window_3 = "k=3"  # +=3
+w_window_5 = "k=5"  # +=5
+w_window_10 = "k=10"  # +=10
 
+# sense frequency based on features
 feature_sense_dict = {w_plus_1: {}, w_minus_1: {}, w_pair_minus_2: {}, w_pair_plus_2: {},
                       w_pair_minus_plus_1: {}, w_window_3: {}, w_window_5: {}, w_window_10: {}}
-feature_frequency_dict = {}
-sense_frequency_dict = {sense_phone: 0, sense_product: 0}
+feature_frequency_dict = {}  # feature frequency
+sense_frequency_dict = {sense_phone: 0, sense_product: 0}  # sense frequency
 
-ranked_tests = []
-answers = []
+ranked_tests = []  # ranked tests
+answers = []  # answers to test set
 
 
+# gets rid of unnecessary elements in context and returns a tokenized version
 def clean_context(content):
     content = re.sub(r'(<.>|<\/.>|\.|,|;|!|-|&|\)|\(|\"|\?)', ' ', content)
     content = re.sub(r'\s+', ' ', content)
-    # content = re.sub(r'\s(of|a|the|is|was|in|an|are|at|but|you)\s', ' ', content)
-    # content = re.sub(r'\s+', ' ', content)
     content = re.sub(">lines<", ">line<", content)
     return content.split()
 
 
+# adds and updates counts of senses according to features
 def add_to_feature_sense_dict(w_feature, feature_word):
     if feature_word not in feature_sense_dict[w_feature].keys():
         feature_sense_dict[w_feature][feature_word] = {sense_phone: 1, sense_product: 1}
@@ -60,17 +76,21 @@ def add_to_feature_sense_dict(w_feature, feature_word):
     feature_sense_dict[w_feature][feature_word][sense] += 1
 
 
+# gets all unigrams within a window and updates frequencies
 def add_with_window_size(context_list, w_feature, k, target_ind, add_frequencies):
     for ind in range(target_ind - k, target_ind + k):
+        # skip index if out of range or is target
         if ind < 0 or ind >= len(context_list) or ind == target_ind:
             continue
 
         add_to_feature_sense_dict(w_feature, context_list[ind])
 
+        # updates frequencies
         if add_frequencies:
             increment_feature_frequency(context_list[ind])
 
 
+# adds and updates counts of features
 def increment_feature_frequency(feature_term):
     if feature_term in feature_frequency_dict.keys():
         feature_frequency_dict[feature_term] += 1
@@ -78,20 +98,26 @@ def increment_feature_frequency(feature_term):
         feature_frequency_dict[feature_term] = 1
 
 
+# creates test based on info from ranked tests list and returns if the test was a success or a failure
 def create_test(test_number, feature_term, is_list):
-    test_feature = ranked_tests[test_number]
+    test_feature = ranked_tests[test_number]  # test info according to number
 
+    # if training feature is in tested feature list then success
     if is_list and test_feature[2] in feature_term:
         return True
+    # if tested feature is same as training feature then success
     elif not is_list and feature_term == test_feature[2]:
         return True
 
+    # if test failed then failure
     return False
 
 
+# creates list of features of window size from test data
 def create_test_feature_list(context_list, k, target_ind):
     features_list = []
     for i in range(target_ind - k, target_ind + k):
+        # skip if out of range or is target
         if i < 0 or i >= len(context_list) or i == target_ind:
             continue
         features_list.append(context_list[i])
@@ -99,80 +125,103 @@ def create_test_feature_list(context_list, k, target_ind):
     return features_list
 
 
+# tokenize training set by instance and pop off useless last bit
 with open(train_set, 'r', encoding="utf-8-sig") as file:
     train_content.extend(file.read().lower().split("</instance>"))
     train_content.pop()
 
+# tokenize test set by instance and pop off useless last bit
 with open(test_set, 'r', encoding="utf-8-sig") as file:
     test_content.extend(file.read().lower().split("</instance>"))
     test_content.pop()
 
+# for training set
 for instance in train_content:
+    # get sense and update count
     sense = re.search(r'senseid=\"(.*)\"', instance).group(1)
     sense_frequency_dict[sense] += 1
 
+    # get context then clean and tokenize it
     context = re.search(r'<context>\n(.*)\n</context>', instance).group(1)
     context_words = clean_context(context)
 
-    target = context_words.index("<head>line</head>")
+    target = context_words.index("<head>line</head>")  # index of target
 
+    # get features of type w+1
     if target + 1 < len(context_words):
         add_to_feature_sense_dict(w_plus_1, context_words[target + 1])
 
+    # get features of type w-1
     if target - 1 >= 0:
         add_to_feature_sense_dict(w_minus_1, context_words[target - 1])
 
+    # get features of type w-2 & w-1
     if target - 2 >= 0:
         bigram = context_words[target - 2] + " " + context_words[target - 1]
         add_to_feature_sense_dict(w_pair_minus_2, bigram)
         increment_feature_frequency(bigram)
 
+    # get features of type w+1 & w+2
     if target + 2 < len(context_words):
         bigram = context_words[target + 1] + " " + context_words[target + 2]
         add_to_feature_sense_dict(w_pair_plus_2, bigram)
         increment_feature_frequency(bigram)
 
+    # get features of type w-1 & w+1
     if target - 1 >= 0 and target + 1 < len(context_words):
         bigram = context_words[target - 1] + " " + context_words[target + 1]
         add_to_feature_sense_dict(w_pair_minus_plus_1, bigram)
         increment_feature_frequency(bigram)
 
+    # get features of window sizes 3, 5, and 10
     add_with_window_size(context_words, w_window_3, 3, target, False)
     add_with_window_size(context_words, w_window_5, 5, target, False)
     add_with_window_size(context_words, w_window_10, 10, target, True)
 
+# calculate log-likelihood ratio for each feature
 for feature_type, feature_list in feature_sense_dict.items():
     for feature, frequency in feature_list.items():
+        # probability of phone given feature
         prob_phone = frequency[sense_phone] / feature_frequency_dict[feature]
+        # probability of product given feature
         prob_product = frequency[sense_product] / feature_frequency_dict[feature]
 
+        # pick sense with higher probability
         if prob_phone >= prob_product:
             sense = sense_phone
         else:
             sense = sense_product
 
+        # abs(log( (P(sense_1|feature)/(P(sense_2|feature) ))
         log_likelihood_ratio = round(abs(math.log(prob_phone / prob_product)), 5)
 
+        # add calculated ratio, feature type, feature, and sense to test
         test = (log_likelihood_ratio, feature_type, feature, sense)
         ranked_tests.append(test)
 
+# sort tests in descending order
 ranked_tests.sort(key=lambda x: x[0], reverse=True)
 
+# write info to model log
 with open(model, 'w', encoding="utf-8-sig") as file:
     for data in ranked_tests:
         line = "feature type = " + data[1] + ", feature = " + data[2] + ", log-likelihood score = " + str(
                 data[0]) + ", sense = " + data[3] + "\n"
         file.write(line)
 
+# for test set
 for instance in test_content:
+    # get instance id
     instance_id = re.search(r'instance id=\"(.*)\"', instance).group(1)
 
+    # get context then clean and tokenize it
     context = re.search(r'<context>\n(.*)\n</context>', instance).group(1)
     context_words = clean_context(context)
 
-    target = context_words.index("<head>line</head>")
+    target = context_words.index("<head>line</head>")  # index of target
 
-    sense = ""
+    sense = ""  # chosen sense
+    # going through tests in order and if there is a success, assign associated sense to target and break
     for ind, test in enumerate(ranked_tests):
         if test[1] == w_plus_1 and target + 1 < len(context_words):
             if create_test(ind, context_words[target + 1], False):
@@ -207,14 +256,17 @@ for instance in test_content:
                 sense = test[3]
                 break
 
+    # if no sense was assigned and every test failed, assign the most frequent sense to target
     if sense == "":
         if sense_frequency_dict[sense_phone] >= sense_frequency_dict[sense_product]:
             sense = sense_phone
         else:
             sense = sense_product
 
+    # format answer and add to list
     answer = "<answer instance=\"" + instance_id + "\" senseid=\"" + sense + "\"/>"
     answers.append(answer)
 
+# print answers
 for answer in answers:
     print(answer)
